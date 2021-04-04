d8fa2c1a34e24aa42123a03c307c177971c3d9e2
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-04-04 10:51:02 -0300

    fix: fix questions validation

--- js/index.js
+++ js/index.js
@@ -106,7 +106,7 @@ request.onload = function () {
 
   function InputLoop(add = true) {
     for (let c = 1; c < document.querySelectorAll("form").length + 1; c++) {
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < document.querySelectorAll("form").item(c - 1).length; i++) {
         add ? document.querySelectorAll(`input[name='question${c}']`).item(i).addEventListener("click", Permission) : document.querySelectorAll(`input[name='question${c}']`).item(i).removeEventListener("click", Permission);
       };
     };
@@ -154,7 +154,8 @@ request.onload = function () {
     };
   };
 
-  var total = 0, co = 0, count = 0, SecondCheck = false, ThirdCheck = false;
+  var total = 0, co = 0, NumberOfForms = 0, SecondCheck = false, ThirdCheck = false;
+  NumberOfInput = 0;
 
   function exchange(variable, text, color = 'black') {
     variable.style.color = color;
@@ -185,31 +186,44 @@ request.onload = function () {
         if (co == 5) {
           clearInterval(Interval);
 
+          var ValueOfQuiz = 0;
+          var Hits = 0;
+
           for (let c = 0; c < document.querySelectorAll("input").length - 1; c++) {
             var title = title;
             var points = points;
             var label = document.querySelectorAll("label").item(c);
             var input = document.querySelectorAll("input").item(c);
 
-            if (c + 1 == NumberOfAllAlternatives[0] - 1) {
-              count += 1;
-              title = document.querySelectorAll("h1").item(count);
-              points = document.querySelector(`#q${count}`);
-              NumberOfAllAlternatives.splice(0, 1);
+            if (NumberOfInput == 0) {
+              NumberOfForms += 1;
+              title = document.querySelectorAll("h1").item(NumberOfForms);
+              points = document.querySelector(`#q${NumberOfForms}`);
             };
 
-            if (CorrectQuestions.indexOf(c) != -1 && title != undefined) {
+            NumberOfInput += 1;
+
+            if (CorrectQuestions.indexOf(c) != -1) {
+              ValueOfQuiz += CorrectQuestionsValue[0];
+
               if (input.checked) {
-                total += 2;
+                total += CorrectQuestionsValue[0];
                 title.style.color = '#1E8E3E';
-                points.textContent = '2/2';
+                points.textContent = `${CorrectQuestionsValue[0]}/${CorrectQuestionsValue[0]}`;
+                Hits += 1;
               };
               label.style.background = '#E6F4EA';
 
-            } else if (input.checked && title != undefined) {
+            } else if (input.checked) {
               title.style.color = '#D93025';
               label.style.background = '#FCE8E6';
-              points.textContent = '0/2';
+              points.textContent = `0/${CorrectQuestionsValue[0]}`;
+            };
+
+            if (NumberOfInput == NumberOfAllAlternatives[0]) {
+              NumberOfInput = 0;
+              NumberOfAllAlternatives.splice(0, 1);
+              CorrectQuestionsValue.splice(0, 1);
             };
           };
 
@@ -217,11 +231,11 @@ request.onload = function () {
           var Old3 = document.createElement("h1");
 
           Old2.id = 'txt3';
-          Old.textContent = `Nota: ${total}/${CorrectQuestions.length * 2} pontos.`;
-          Old2.textContent = `Acertos: ${total / 2}/${CorrectQuestions.length} questões.`;
+          Old.textContent = `Nota: ${total}/${ValueOfQuiz} pontos.`;
+          Old2.textContent = `Acertos: ${Hits}/${CorrectQuestions.length} questões.`;
 
-          total >= 8 ? Old.style.color = Old2.style.color = 'darkblue' : Old.style.color = Old2.style.color = '#D93025';
-          total >= 8 ? Old3.textContent = 'Parabéns! Mandou bem! :)' : Old3.textContent = 'Não foi desta vez... :(';
+          total >= ValueOfQuiz / 2 + 1 ? Old.style.color = Old2.style.color = 'darkblue' : Old.style.color = Old2.style.color = '#D93025';
+          total >= ValueOfQuiz / 2 + 1 ? Old3.textContent = 'Parabéns! Mandou bem! :)' : Old3.textContent = 'Não foi desta vez... :(';
 
           [Old2, Old3].forEach(function (array) { document.querySelectorAll("section").item(1).querySelectorAll("h1").item(document.querySelectorAll("section").item(1).querySelectorAll("h1").length - 1).after(array) });
         } else {
